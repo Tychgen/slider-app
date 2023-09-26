7a2b75ac4a685d29263b660d11974ed308fb47b0
Tychgen <alikxelefov@gmail.com>
2023-09-26 11:18:28 +0400

    Display slide Images function created

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,42 @@ const leftButton: HTMLButtonElement | null = document.getElementById('left-butto
 const playPauseButton: HTMLButtonElement | null = document.getElementById('play-pause-button') as HTMLButtonElement;
 const rightButton = document.getElementById('right-button') as HTMLButtonElement;
 let pause: boolean = false;
+
+async function displayCatImages() {
+  try {
+    const catImages: CatImage[] = await getCatImages(); 
+    slides.forEach((slide) => {
+      if (slider) {
+        slider.removeChild(slide);
+      }
+    });
+
+    catImages.forEach((catImage: CatImage, index: number) => {
+      const slide = document.createElement('div');
+      slide.className = 'slide';
+      if (index === 0) {
+        slide.classList.add('active');
+      }
+
+      const img = document.createElement('img');
+      img.src = catImage.url;
+      img.alt = `Image ${index + 1}`;
+
+      const desc = document.createElement('div');
+      desc.className = 'desc';
+      desc.textContent = `Image ${index + 1}`;
+
+      slide.appendChild(img);
+      slide.appendChild(desc);
+      if (slider) {
+        slider.appendChild(slide);
+      }
+    });
+
+    slides = document.querySelectorAll('.slide');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+displayCatImages();
