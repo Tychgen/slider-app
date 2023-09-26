18a315c75b4f43fad060d9c10cef4a30b254da67
Tychgen <alikxelefov@gmail.com>
2023-09-26 11:12:31 +0400

    Created Type for API. Async Fetch implemented.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+interface CatImage {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+async function getCatImages(): Promise<CatImage[]> {
+  try {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+    const data: CatImage[] = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
