54b0d0b9d8a1c11156e34ddc4a2b094c18940039
Tychgen <alikxelefov@gmail.com>
2023-09-26 11:16:31 +0400

    DOM tags selected

--- src/index.ts
+++ src/index.ts
@@ -18,4 +18,9 @@ async function getCatImages(): Promise<CatImage[]> {
 }
 
 let i: number = 0;
+let slides: NodeListOf<HTMLDivElement> = document.querySelectorAll('.slide');
+let slider: HTMLDivElement | null = document.querySelector('.slider');
+const leftButton: HTMLButtonElement | null = document.getElementById('left-button') as HTMLButtonElement;
+const playPauseButton: HTMLButtonElement | null = document.getElementById('play-pause-button') as HTMLButtonElement;
+const rightButton = document.getElementById('right-button') as HTMLButtonElement;
 let pause: boolean = false;
